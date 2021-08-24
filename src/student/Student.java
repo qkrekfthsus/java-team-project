3970e6004b962c5665ada29801658e0173f454dc
jaehomoon <answogh123@naver.com>
2021-08-24 16:43:40 +0900

    A

--- src/student/Student.java
+++ src/student/Student.java
@@ -1,9 +1,20 @@
 package student;
 
 public class Student {
-	int student_id, student_grade;
+	String student_id, student_grade;
 	String student_name, student_major, student_tel, student_state;
 	
+	public Student(String student_id, String student_grade, String student_name, String student_major, String student_tel,
+			String student_state) {
+		super();
+		this.student_id = student_id;
+		this.student_grade = student_grade;
+		this.student_name = student_name;
+		this.student_major = student_major;
+		this.student_tel = student_tel;
+		this.student_state = student_state;
+	}
+	/*
 	public Student(String[] studentInfo) {
 		this.student_id = Integer.parseInt(studentInfo[0]);
 		this.student_grade = Integer.parseInt(studentInfo[1]);
@@ -11,7 +22,7 @@ public class Student {
 		this.student_major = studentInfo[3];
 		this.student_tel = studentInfo[4];
 		this.student_state = studentInfo[5];
-	}
+	}*/
 	
 	// 20170815/4/최무열/남/컴퓨터공학과/010-0456-4692/졸업
 }
--- src/student/StudentManagement.java
+++ src/student/StudentManagement.java
@@ -1,6 +1,20 @@
 package student;
 
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class StudentManagement {
+	static Scanner scanner = new Scanner(System.in);
+	static String number;
+	
+	static int savepoint;
+	
 	public static void registerStudent() {
 		
 	}
@@ -9,8 +23,90 @@ public class StudentManagement {
 		
 	}
 	
-	public static void changeStudent() {
+	public static void changeStudent() throws IOException {
+		File myFile = new File("strudent.txt");
+		BufferedReader reader = new BufferedReader(new FileReader(myFile));
 		
+		String line =null;
+		int n=0;
+		ArrayList list=new ArrayList();
+		
+		  while((line=reader.readLine()) != null ){
+		   n++;
+		   list.add(line);
+		  }
+		  reader.close();
+		  
+		  Student[] Student=new Student[n];
+		  
+		  for(int i=0; i<n; i++){
+		   String tok=(String)list.get(i);
+		   String[] token=tok.split("\t");
+		   Student[i]=new Student(token[0],token[1], token[2], token[3], token[4], token[5]);
+		 
+		  }
+		while(true) {
+		System.out.println("====================================================");
+		System.out.println("인적사항을 변경할 학생의 학번을 입력해주세요.");
+		System.out.println("====================================================");
+		number = scanner.nextLine();
+		for(int i=0; i<n; i++){
+			if(number.equals(Student[i].student_id)) {
+			System.out.println("====================================================");
+			System.out.println(Student[0].student_id+"	 "+Student[0].student_grade+" 	"+Student[0].student_name+" 	"+Student[0].student_major+"	 "+Student[0].student_tel+"		"+Student[0].student_state);
+			System.out.println(Student[i].student_id+" "+Student[i].student_grade+" 	"+Student[i].student_name+" 	"+Student[i].student_major+" "+Student[i].student_tel+"	 "+Student[i].student_state);
+			System.out.println("인적 사항을 변경 하시겠습니까?(y/n)");
+			System.out.println("====================================================");
+			savepoint = i;
+			}
+		}
+		number = scanner.nextLine();
+		
+		if(number.equals("y")) {
+		System.out.println("====================================================");
+		System.out.println("변경할 항목을 입력해주세요.");
+		System.out.println("====================================================");
+		number = scanner.nextLine();
+			if(number.equals(Student[0].student_id)) {
+				System.out.println("학번은 변경할수 없습니다");
+				
+			}
+			if(number.equals(Student[0].student_grade)) {
+				System.out.println("변경할 "+Student[0].student_grade+"을 입력해주세요. 현재학년은 " +Student[savepoint].student_grade+"입니다.");
+				number = scanner.nextLine();
+				Student[savepoint].student_grade = number;
+				//System.out.println(System.out.println(Student[savepoint].student_id+" "+Student[savepoint].student_grade+" 	"+Student[savepoint].student_name+" 	"+Student[savepoint].student_major+" "+Student[savepoint].student_tel+"	 "+Student[savepoint].student_state);
+				
+			}
+			if(number.equals(Student[0].student_name)) {
+				
+			}
+			if(number.equals(Student[0].student_major)) {
+				
+			}
+			if(number.equals(Student[0].student_tel)) {
+	
+			}
+			if(number.equals(Student[0].student_state)) {
+	
+			}
+			else {
+				System.out.println("다시입력해주세요");
+				continue;
+			}
+			
+			
+		}
+		if(number=="n") {
+			break;
+		}
+		
+		else {
+			System.out.println("다시입력해주세요.");
+			continue;
+		}
+			
+		}
 	}
 	
 	public static void gradeManagement() {

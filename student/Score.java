69d9de7c6c777918a69e9325de6cc2e746816f79
yerim <whitepurple@sungkyul.ac.kr>
2021-08-25 17:12:48 +0900

    FileManager 병합, Subject파일 Score로 변경

---
+++ student/Score.java
@@ -0,0 +1,20 @@
+package student;
+
+public class Score {
+	int student_id;
+	String subject_code, subject_name, type, professor, score;
+	
+	public Score(String[] scoreInfo) {
+		this.student_id = student_id;
+		this.subject_code = subject_code;
+		this.subject_name = subject_name;
+		this.type = type;
+		this.professor = professor;
+		this.score = score;
+	}
+	
+	
+	
+	
+	// 201xxxxx/과목명/과목 코드/구분/담당 교수/성적
+}

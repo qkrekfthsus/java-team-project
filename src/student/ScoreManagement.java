275251f984458e7ce68f3cc91691c93705fa0211
yerim <whitepurple@sungkyul.ac.kr>
2021-08-26 18:04:11 +0900

    develop 다운 받은 그대로

--- src/student/ScoreManagement.java
+++ src/student/ScoreManagement.java
@@ -11,7 +11,7 @@ public class ScoreManagement {
 
 		while(true) {
 			System.out.println("========================================================");
-			System.out.println("sfjsdfsdfsdkjf원하는 기능을 선택해주세요.( 1~5 번호를 입력해주세요. )");
+			System.out.println("원하는 기능을 선택해주세요.( 1~5 번호를 입력해주세요. )");
 			System.out.println("1.학생 등록  2.학생 정보 조회  3.학생 정보 수정 4.성적 관리  5.종료");
 			System.out.println("========================================================");
 			MainMenuNum = Integer.parseInt(scanner.nextLine());
@@ -58,7 +58,7 @@ public class ScoreManagement {
 		
 		if(readForStudent != null) {
 			if(!readForStudent.score.isEmpty()) {
-				for(Score grade : readForStudent.score) {
+				for(Score score : readForStudent.score) {
 					System.out.println("----------------------------------------------------------------------");
 					System.out.println();
 					System.out.println("----------------------------------------------------------------------");
